a7f3f106d2528bb5d9c0ef1cbb79bb193453affc
chernetsky <chernetsky@gmail.com>
2016-12-22 14:41:19 +0800

    Added css files

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
+    app.import('vendor/anychart-fonts.css');
+    app.import('vendor/anychart-ui.min.css');
     app.import('vendor/anychart.min.js');
     //app.import(app.bowerDirectory + '/anychart/dist/anychart.min.js');
     // https://cdn.anychart.com/js/latest/anychart.min.js
