580fd1149b1a3f261b65cd02857c3e77842e071d
chernetsky <chernetsky@gmail.com>
2016-12-26 17:11:56 +0800

    fix

--- addon/components/ember-anychart.js
+++ addon/components/ember-anychart.js
@@ -1,50 +1,54 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
-  classNames: ["ember-anychart"],
+    classNames: ["ember-anychart"],
 
-  // Instance of chart or stage object
-  instance: undefined,
+    // Instance of chart or stage object
+    instance: undefined,
 
-  didReceiveAttrs() {
-    this._super(...arguments);
+    didReceiveAttrs() {
+        this._super(...arguments);
 
-    let attributes = arguments[0]["newAttrs"];
+        let attributes = arguments[0]["newAttrs"];
 
-    if(!("instance" in attributes)) {
-      let acType = this.get("type") || "line";
-      this.set("instance", anychart[acType]());
-    }
-
-    let instance = this.get("instance");
-    let attr;
-    for (attr in attributes) {
-      let value = attributes[attr]["value"];
-      if(typeof instance[attr] === "function") {
-        instance[attr](value);
-      }
-    }
-  },
-
-  didInsertElement: function() {
-    this._super(...arguments);
-
-    let instance = this.get("instance");
-    if (typeof instance === "object") {
-      instance.container(this.elementId);
-      // If instance is a chart
-      if (typeof instance.draw === "function") {
-        instance.draw();
-
-        // after draw callback processing
-        let afterDraw = this.get("afterDraw");
-        if (typeof afterDraw === "function") {
-          instance.listenOnce("chartdraw", function() {
-            instance.animation(false);
-            afterDraw(instance);
-          });
+        if (!("instance" in attributes)) {
+            let acType = this.get("type") || "line";
+            this.set("instance", anychart[acType]());
         }
-      }
+
+        let instance = this.get("instance");
+        let attr;
+        for (attr in attributes) {
+            let value = attributes[attr]["value"];
+            if (typeof instance[attr] === "function") {
+                instance[attr](value);
+            }
+        }
+    },
+
+    didInsertElement() {
+        this._super(...arguments);
+
+        Ember.run.scheduleOnce('afterRender', this, '_renderChart');
+    },
+    _renderChart(){
+        let instance = this.get("instance");
+        if (typeof instance === "object") {
+            instance.container(this.elementId);
+            // If instance is a chart
+            if (typeof instance.draw === "function") {
+                instance.draw();
+
+                // after draw callback processing
+                let afterDraw = this.get("afterDraw");
+                if (typeof afterDraw === "function") {
+                    instance.listenOnce("chartdraw", function () {
+                        instance.animation(false);
+                        afterDraw(instance);
+                    });
+                }
+            }
+        }
     }
-  },
+
 });
