85042f2430b4fb8e3a95a9de423b520c26625780
chernetsky <chernetsky@gmail.com>
2017-01-23 13:44:24 +0800

    Added anychart fonts

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -20,6 +20,11 @@ module.exports = function (defaults) {
    */
     app.import('vendor/anychart/dist/anychart-bundle.min.js');
     app.import('vendor/anychart/dist/anychart-ui.min.css');
+    app.import('vendor/anychart/dist/fonts.css');
+    app.import('vendor/anychart/dist/fonts/anychart.eot', {destDir: '/assets/fonts'});
+    app.import('vendor/anychart/dist/fonts/anychart.svg', {destDir: '/assets/fonts'});
+    app.import('vendor/anychart/dist/fonts/anychart.ttf', {destDir: '/assets/fonts'});
+    app.import('vendor/anychart/dist/fonts/anychart.woff', {destDir: '/assets/fonts'});
 
   return app.toTree();
 };
--- index.js
+++ index.js
@@ -8,6 +8,11 @@ module.exports = {
 
     app.import('vendor/anychart/dist/anychart-bundle.min.js');
     app.import('vendor/anychart/dist/anychart-ui.min.css');
+    app.import('vendor/anychart/dist/fonts.css');
+    app.import('vendor/anychart/dist/fonts/anychart.eot', {destDir: '/assets/fonts'});
+    app.import('vendor/anychart/dist/fonts/anychart.svg', {destDir: '/assets/fonts'});
+    app.import('vendor/anychart/dist/fonts/anychart.ttf', {destDir: '/assets/fonts'});
+    app.import('vendor/anychart/dist/fonts/anychart.woff', {destDir: '/assets/fonts'});
   }
   //,
   // options: {
